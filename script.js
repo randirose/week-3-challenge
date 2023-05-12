4ef8836453ca697adc36d4b19a6847f591a61dbe
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-05-12 14:50:08 -0700

    toUpperCase not working, changed back to upper case string for uppercase prop

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 // Declare object for user's choices on password requirements
 var pwChoices = {
   lowercase: "abcdefghijklmnopqrstuvqxyz",
-  uppercase: this[lowercase].toUpperCase(),
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   number: "0123456789",
   specialChar: "!@#$%^&*"
 };
-console.log(pwChoices.uppercase);
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
 
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
