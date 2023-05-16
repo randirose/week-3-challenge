781d050d8b36cf0c445fa0d859b3019e9a0b53fa
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-05-16 11:55:20 -0700

    added userNumber confirmation variable, but function is still only returning lowercase letters if userLength is less than 26 (or is 26) and then only adding uppercase letters for next 26 (length of uppercase string) before adding numbers. seems to be in order of the string as well, so not randomizing the full concatenated string it seems like

--- script.js
+++ script.js
@@ -8,28 +8,37 @@ var userPasswordChoices = {
 };
 // generatePassword function that will create password based on user choices via prompts and confirmations
 function generatePassword() {
+
 // first, I need to use a prompt to gather data from the user - how long they want their password to be btwn 8 and 128
   var userLength = prompt("How long do you want your password to be? Please enter a number between 8 and 128.")
+
 // if user inputs a number not in the given range, the function needs to kick them out and ask them to start over with a number in the given range
   if (userLength < 8 || userLength > 128) {
     alert("You have chosen an invalid length, please start over.");
     return;
-    // return ends the function right here
+    // return ends the function right here and put 'undefined' in the text box
   }
   else {
+    // rest of the function will go here, once they've selected a valid password length
+
     // declare password variable as empty string, and another variable userPassword to concatenate property strings from above onto before randomizing
     var password = "";
     var userPassword = "";
-    // rest of the function will go here, once they've selected a valid password length
-    // user confirms if they want lowercase letters in their password
+
+    // user confirms if they want lowercase letters in their password, if they do, empty string userPassword concatenates (adds to the end) the lowercase string (property of userPasswordChoices object)
     var userLowercase = confirm("Press 'OK' if you want to include lowercase letters in your password. Press 'CANCEL' if you do not");
     if (userLowercase) {
       userPassword += userPasswordChoices.lowercase;
       };
-    // user confirms if they want uppercase letters in their password
+    // user confirms if they want uppercase letters in their password, if they do, empty string userPassword concatenates (adds to the end) the uppercase string (property of userPasswordChoices object)
     var userUppercase = confirm("Press 'OK' if you want to include uppercase letters in your password. Press 'CANCEL' if you do not");
     if (userUppercase) {
       userPassword += userPasswordChoices.uppercase;
+    }
+    // user confirms if they want numbers in their password, if they do, empty string userPassword concatenates (adds to the end) the numbers string (property of userPasswordChoices object)
+    var userNumber = confirm("Press 'OK' if you want to include numbers in your password. Press 'CANCEL' if you do not");
+    if (userNumber) {
+      userPassword += userPasswordChoices.number;
     }
     for (i = 0; i < userLength; i++) {
       password += userPassword[Math.floor(Math.random() * userLength)]
