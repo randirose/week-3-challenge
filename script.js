8483f907fc650a0bab5db6bd179715c880393b32
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-05-16 10:26:10 -0700

    starting on generatePassword function by prompting user for length, and using an if statement to make sure they've chosen a valid length before continuing with the function

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // Assignment code here
 // Declare object for user's choices on password requirements
-var pwChoices = {
+var pwUserChoices = {
   lowercase: "abcdefghijklmnopqrstuvqxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   number: "0123456789",
   specialChar: "!@#$%^&*"
 };
-
+// generatePassword function that will create password based on user choices via prompts and confirmations
+function generatePassword() {
+// first, I need to use a prompt to gather data from the user - how long they want their password to be btwn 8 and 128
+  var userLength = prompt("How long do you want your password to be? Please enter a number between 8 and 128.")
+// if user inputs a number not in the given range, the function needs to kick them out and ask them to start over with a number in the given range
+  if (userLength < 8 || userLength > 128) {
+    alert("You have chosen an invalid length, please start over.");
+    return;
+    // return ends the function right here
+  }
+  else {
+    // rest of the function will go here, once they've selected a valid password length
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -21,5 +34,5 @@ function writePassword() {
 }
 
 
-// Add event listener to generate button
+// Add event listener to generate button - will run everything once the generate button is clicked since the generatePassword function is called inside the writePassword function
 generateBtn.addEventListener("click", writePassword);
