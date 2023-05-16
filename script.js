f7ad9957579e6d4457b21b00e1891389e338738d
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-05-16 14:07:39 -0700

    added if statement for if the user doesn't select any character types during the confirmation prompts, to return an alert that they must select at least one character type and to start over - kicks user out of the function

--- script.js
+++ script.js
@@ -46,7 +46,10 @@ function generatePassword() {
     if (userSpecialChar) {
       userPassword += userPasswordChoices.specialChar;
     }
-
+    if (userLowercase !== true && userUppercase !== true && userNumber !== true && userSpecialChar !== true) {
+      alert("You must choose at least one character type for your password, please start again.");
+      return;
+    }
     // now that userPassword has concatenated all the strings together (that the user said they wanted to include), we can use it in a with the Math.random function, multiplying it by the full length of the concatenated userPassword string to generate a random string to then use in the for loop, which keeps it to the user-requested length
     for (i = 0; i < userLength; i++) {
       let randomPassword = Math.floor(Math.random() * userPassword.length);
